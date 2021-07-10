060c2a31cff4d4db309a3830c831f0f526433a61
Dimitar Stoyanov <dimitars345@gmail.com>
2021-07-11 00:28:27 +0300

    init new project

---
+++ src/main/java/catclinic/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package catclinic.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CatClinicApplication.class);
+    }
+
+}
---
+++ src/test/java/catclinic/demo/CatClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package catclinic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
